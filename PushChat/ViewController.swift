5b940854b9bbd8c454d6fe50392bd356d5507123
Connor McLaughlin <connor.mclaughlin@yale.edu>
2015-07-24 22:45:04 -0700

    Initial Commit

---
+++ PushChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PushChat
+//
+//  Created by Connor McLaughlin on 7/24/15.
+//  Copyright (c) 2015 Connor McLaughlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
