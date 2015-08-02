131862689fed30bf6cb0eda760c991a3e30f54a0
Connor McLaughlin <connor.mclaughlin@yale.edu>
2015-08-02 10:20:58 -0700

    Adds ability to take a picture with the back camera

--- PushChat/ViewController.swift
+++ PushChat/ViewController.swift
@@ -2,18 +2,26 @@
 //  ViewController.swift
 //  PushChat
 //
-//  Created by Connor McLaughlin on 7/24/15.
+//  Created by Connor McLaughlin on 8/1/15.
 //  Copyright (c) 2015 Connor McLaughlin. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    
+    let captureSession = AVCaptureSession()
+    var previewLayer : AVCaptureVideoPreviewLayer?
+    
+    // If we find a device we'll store it here for later use
+    var captureDevice : AVCaptureDevice?
+    var stillImageOutput : AVCaptureStillImageOutput = AVCaptureStillImageOutput()
+    var capturedImage: UIImage!
+    
+    @IBOutlet weak var takePhotoButton: UIButton!
+    //@IBOutlet weak var viewPhotoButton: UIButton!
+    @IBOutlet weak var retakePhotoButton: UIButton!
     
     override func viewDidAppear(animated: Bool) {
         checkLoggedIn()
@@ -34,12 +42,115 @@ class ViewController: UIViewController {
             defaults.setBool(false, forKey: "loggedInFlag")
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        captureSession.sessionPreset = AVCaptureSessionPresetHigh
+        
+        let devices = AVCaptureDevice.devices()
+        
+        // Loop through all the capture devices on this phone
+        for device in devices {
+            // Make sure this particular device supports video
+            if (device.hasMediaType(AVMediaTypeVideo)) {
+                // Finally check the position and confirm we've got the back camera
+                if(device.position == AVCaptureDevicePosition.Back) {
+                    captureDevice = device as? AVCaptureDevice
+                    if captureDevice != nil {
+                        println("Capture device found")
+                        beginSession()
+                    }
+                }
+            }
+        }
+        //self.viewPhotoButton.hidden = true
+        self.retakePhotoButton.hidden = true
+    }
+    
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+//        if (segue.identifier == "passImage") {
+//            var svc = segue.destinationViewController as! ImageViewController;
+//            
+//            svc.passedImage = self.capturedImage
+//            println("captured image:")
+//            println(self.capturedImage)
+//        }
+//    }
+    
+    @IBAction func retakePhoto(sender: AnyObject) {
+        //self.capturedImage = nil
+        self.takePhotoButton.hidden = false
+        //self.viewPhotoButton.hidden = true
+        self.retakePhotoButton.hidden = true
+        
+        self.captureSession.startRunning()
+    }
+    
+    @IBAction func takePhoto(sender: AnyObject) {
+        //        var videoConnection:AVCaptureConnection = stillImageOutput.connectionWithMediaType(AVMediaTypeVideo)
+        
+        // we do this on another thread so that we don't hang the UI
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            //find the video connection
+            var videoConnection : AVCaptureConnection?
+            for connecton in self.stillImageOutput.connections {
+                //find a matching input port
+                for port in connecton.inputPorts!{
+                    if port.mediaType == AVMediaTypeVideo {
+                        videoConnection = connecton as? AVCaptureConnection
+                        break //for port
+                    }
+                }
+                
+                if videoConnection  != nil {
+                    break// for connections
+                }
+            }
+            if videoConnection  != nil {
+                self.stillImageOutput.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler: {buffer, error -> Void in
+                    if error != nil {
+                        
+                    }
+                    if buffer == nil {
+                        return
+                    }
+                    //println(buffer)
+                    let imageDataJpeg = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(buffer)
+                    self.capturedImage = UIImage(data: imageDataJpeg)!
+                    self.captureSession.stopRunning()
+                })
+                //self.captureSession.stopRunning()
+            }
+        }
+        self.takePhotoButton.hidden = true
+        //self.viewPhotoButton.hidden = false
+        self.retakePhotoButton.hidden = false
+    }
+    
+    func beginSession() {
+        var err : NSError? = nil
+        captureSession.addInput(AVCaptureDeviceInput(device: captureDevice, error: &err))
+        
+        if err != nil {
+            println("error: \(err?.localizedDescription)")
+        }
+        
+        captureSession.addOutput(self.stillImageOutput)
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        //self.view.layer.addSublayer(previewLayer)
+        self.view.layer.insertSublayer(previewLayer, atIndex: 0)
+        previewLayer?.frame = self.view.layer.frame
+        captureSession.startRunning()
+    }
+    
+    
 }
 
